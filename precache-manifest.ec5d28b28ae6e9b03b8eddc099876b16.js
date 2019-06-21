7a35dcd799c499549a8639c7328405e3d3a154a6
Akash Srivastava <akashom53@gmail.com>
2019-06-21 14:13:26 +0530

    Updates

---
+++ precache-manifest.ec5d28b28ae6e9b03b8eddc099876b16.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a20cf7d491eb8837e6eae159bed9e016",
+    "url": "/ScreenshotGenerator/index.html"
+  },
+  {
+    "revision": "ad7c8893f6ecf3ba517f",
+    "url": "/ScreenshotGenerator/static/css/main.359b6a0c.chunk.css"
+  },
+  {
+    "revision": "937a13fc2b0211c90cf1",
+    "url": "/ScreenshotGenerator/static/js/2.2fc83c1a.chunk.js"
+  },
+  {
+    "revision": "ad7c8893f6ecf3ba517f",
+    "url": "/ScreenshotGenerator/static/js/main.7871fd1a.chunk.js"
+  },
+  {
+    "revision": "746b8284782c61826a6a",
+    "url": "/ScreenshotGenerator/static/js/runtime~main.0f8deac3.js"
+  },
+  {
+    "revision": "85eec3f8c3e9c2f35f72e57bdef9466e",
+    "url": "/ScreenshotGenerator/static/media/iphone_1.85eec3f8.png"
+  },
+  {
+    "revision": "a2651bc375b41fc1c7b98af4244df898",
+    "url": "/ScreenshotGenerator/static/media/iphone_2.a2651bc3.jpg"
+  }
+]);
